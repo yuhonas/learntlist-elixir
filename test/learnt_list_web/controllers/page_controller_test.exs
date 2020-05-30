0d17e0973f6e54aec61e331067fa2ebb188e42b4
Clint Plummer <clint@yuhonas.com>
2020-05-31 09:26:30 +1000

    add bootstrap + header page

--- test/learnt_list_web/controllers/page_controller_test.exs
+++ test/learnt_list_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule LearntListWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Like to Learn?"
   end
 end
